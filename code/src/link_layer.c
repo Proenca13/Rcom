2990e8d427f1a0673b9ef797ab544143832b1c86
Eduardo <edumrcunha@gmail.com>
2024-10-19 18:43:25 +0100

    llread in progress

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -130,20 +130,20 @@ int llwrite(LinkLayer connectionParameters,const unsigned char *buf, int bufSize
 {
     int fd = openSerialPort(connectionParameters.serialPort,connectionParameters.baudRate);
     if(fd < 0)return -1;
-    int frame_size = 6+bufSize;
+    int frame_size = 6 + bufSize;
     unsigned char frame[frame_size];
     frame[0] = FLAG;
     frame[1] = A_trans;
     if(frame_number == 0)frame[2] = 0x00;
     else frame[2] = 0x80;
-    frame[3] = A_trans ^C_SET;
-    memcpy(frame+4,buf, bufSize);
+    frame[3] = A_trans ^ C_SET;
+    memcpy(frame + 4,buf, bufSize);
     unsigned char BCC2  = buf[0];
     for(int i = 1;i<= bufSize;i++){
         BCC2 = BCC2 ^ buf[i];
     }
-    frame[4+bufSize] = BCC2;
-    frame[5+bufSize] = FLAG;
+    frame[4 + bufSize] = BCC2;
+    frame[5 + bufSize] = FLAG;
     alarmCount = 0;
     int accepted = 0;
     State state = START;
@@ -199,12 +199,78 @@ int llwrite(LinkLayer connectionParameters,const unsigned char *buf, int bufSize
 ////////////////////////////////////////////////
 // LLREAD
 ////////////////////////////////////////////////
-int llread(unsigned char *packet)
-{
-    // TODO
 
-    return 0;
+int llread(unsigned char *packet) {
+    State state = START;
+    unsigned char byte;
+    unsigned char C_byte;
+    int packetIndex = 0;
+    unsigned char BCC2 = 0;
+
+    // Iterate through the packet array until the frame is fully processed
+    while (state != READ) {
+        byte = packet[packetIndex++];  // Read the next byte from the packet
+
+        switch (state) {
+            case START:
+                if (byte == FLAG)
+                    state = FLAG_ST;  // Move to FLAG_ST when FLAG is received
+                break;
+
+            case FLAG_ST:
+                if (byte == A_trans)
+                    state = A;  // Move to A if Address (A_trans) matches
+                else if (byte != FLAG)
+                    state = START;  // Reset to START if byte is invalid
+                break;
+
+            case A:
+                if (byte == 0x00 || byte == 0x80) {  // Expected control bytes (frame number)
+                    C_byte = byte;
+                    state = C;  // Move to C when control byte is valid
+                } else if (byte == FLAG)
+                    state = FLAG_ST;  // Restart from FLAG_ST if FLAG is received again
+                else
+                    state = START;  // Invalid byte, reset to START
+                break;
+
+            case C:
+                if (byte == (A_trans ^ C_byte))
+                    state = BCC;  // Move to BCC if XOR of A and C is correct
+                else if (byte == FLAG)
+                    state = FLAG_ST;  // Restart from FLAG_ST if FLAG is received
+                else
+                    state = START;  // Invalid BCC, reset to START
+                break;
+
+            case BCC:
+                if (byte == FLAG) {
+                    state = READ;  // Received final FLAG, frame is valid
+                } else {
+                    if (packetIndex == 1) {
+                        BCC2 = byte;  // Initialize BCC2 with the first byte of the packet
+                    } else {
+                        BCC2 ^= byte;  // XOR each subsequent byte to calculate BCC2
+                    }
+                }
+                break;
+
+            default:
+                state = START;  // Reset to START in case of unexpected state
+                break;
+        }
+    }
+
+    // Perform BCC2 check
+    if (BCC2 != 0) {
+        printf("Error: BCC2 mismatch.\n");
+        return -1;  // Return error if BCC2 check fails
+    }
+
+    printf("Frame successfully received.\n");
+    return packetIndex;  // Return the number of bytes processed from the packet
 }
+
 
 ////////////////////////////////////////////////
 // LLCLOSE
